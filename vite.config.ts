479a64fa1f1e625f7702a1c990c909524ee33328
[dyad] <karl_kieran@yahoo.fr>
2025-11-06 06:01:53 -0500

    [dyad] add Dyad component tagger

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+import dyadComponentTagger from '@dyad-sh/react-vite-component-tagger';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
+  plugins: [dyadComponentTagger(), 
     react(),
     VitePWA({
       registerType: 'autoUpdate',
